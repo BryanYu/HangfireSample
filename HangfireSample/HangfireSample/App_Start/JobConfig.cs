1c25f0e9029bb3f818d1553f172e6481c8e5928e
BryanYu <ak78912@hotmail.com>
2018-09-03 23:22:51 +0800

    HangfireSample

---
+++ HangfireSample/HangfireSample/App_Start/JobConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Hangfire;
+using Hangfire.Server;
+
+namespace HangfireSample.App_Start
+{
+    public class JobConfig
+    {
+        public static void Register()
+        {
+            // fire and got:站台啟動後只會執行一次
+            BackgroundJob.Enqueue(() => Console.WriteLine("Fire and forgot"));
+
+            // delay: 設定時間間隔，每隔時間間隔執行一次
+            BackgroundJob.Schedule(() => Console.WriteLine("Delayed"), TimeSpan.FromDays(1));
+
+            // recurring: 設定cron敘述，重複執行多次
+            RecurringJob.AddOrUpdate(() => Console.WriteLine("Daily Job"), Cron.Daily);
+
+            // continue: 在某個job執行完後接續執行
+            var id = BackgroundJob.Enqueue(() => Console.WriteLine("Hello, "));
+            BackgroundJob.ContinueWith(id, () => Console.WriteLine("world!"));
+
+            BackgroundJob.Enqueue(() => TestFail());
+        }
+
+        public static void TestFail()
+        {
+            var i = 0;
+            var result = 1 / i;
+        }
+    }
+}
---
+++ HangfireSample/HangfireSample/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using Hangfire;
+using HangfireSample.App_Start;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(HangfireSample.Startup))]
+
+namespace HangfireSample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["HangfireConnection"].ConnectionString;
+            GlobalConfiguration.Configuration.UseSqlServerStorage(connectionString);
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+            JobConfig.Register();
+        }
+    }
+}
